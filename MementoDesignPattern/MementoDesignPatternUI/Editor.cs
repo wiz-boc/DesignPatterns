13ecf28f429208508f211b091e143a0349f08dec
Kenroy 'wiz' Gayle <wiz_kidz@outlook.com>
2022-10-09 19:19:20 -0500

    Intial commit

---
+++ MementoDesignPattern/MementoDesignPatternUI/Editor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace MementoDesignPatternUI
+{
+    public class TextEdit {
+        public Editor editor;
+
+        public EditorState<Editor> GetData() {
+            return new EditorState<Editor>(editor);
+        }
+
+        public void restore(EditorState<Editor> state) {
+            editor = state.GetData();
+        }
+
+        public Editor GetEditor() {
+            return editor;
+        }
+    }
+
+	public class Editor
+	{
+		public string Content { get; set; }
+        public string FontName { get; set; }
+        public int FontSize { get; set; }
+
+    }
+
+    public class EditorState<T>
+    {
+        private T Data { get; set; }
+
+        public EditorState(T data)
+        {
+            this.Data = data; 
+        }
+
+        public T GetData() { return Data; }
+
+    }
+
+    public class History<T> {
+        private List<T> states = new List<T>();
+        public void Push(T state) {
+            states.Add(state);
+        }
+        public T Pop() {
+            int lastIndex = states.Count() - 1;
+            T lastState = states[lastIndex];
+            states.Remove(lastState);
+            return lastState;
+        }
+    }
+}
+
---
+++ MementoDesignPattern/MementoDesignPatternUI/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using MementoDesignPatternUI;
+
+TextEdit text = new TextEdit();
+text.editor = new Editor();
+text.editor.Content = "Ok this works";
+text.editor.FontName = "Sague UI";
+text.editor.FontSize = 15;
+Console.WriteLine(text.editor.FontName);
+
+History<EditorState<Editor>> history = new History<EditorState<Editor>>();
+history.Push(text.GetData());
+
+
+text.editor = new Editor();
+text.editor.Content = "Ok this works";
+text.editor.FontName = "Segue UI";
+text.editor.FontSize = 15;
+history.Push(text.GetData());
+Console.WriteLine(text.editor.FontName);
+
+text.editor = new Editor();
+text.editor.Content = "Ok this works";
+text.editor.FontName = "Segue UI";
+text.editor.FontSize = 8;
+history.Push(text.GetData());
+Console.WriteLine(text.editor.FontSize.ToString());
+
+
+text.editor = new Editor();
+text.editor.Content = "Well testing this";
+text.editor.FontName = "Times";
+text.editor.FontSize = 6;
+history.Push(text.GetData());
+Console.WriteLine(text.editor.FontSize.ToString()+" : "+ text.editor.FontName);
+
+Console.WriteLine("Undo Press");
+
+text.restore(history.Pop());
+Console.WriteLine(text.editor.FontSize.ToString() + " : " + text.editor.FontName);
+
+text.restore(history.Pop());
+Console.WriteLine(text.editor.FontSize.ToString() + " : " + text.editor.FontName);
+
+Console.WriteLine("Done!");
+Console.ReadLine();
+
